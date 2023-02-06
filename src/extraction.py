137caa6521707b04a1891b649600d59f9344fae5
imalanz <arq.imalanz@gmail.com>
2023-02-06 21:26:04 +0100

    oralepues

--- src/extraction.py
+++ src/extraction.py
@@ -31,23 +31,14 @@ def delete_nutrient (x):
 def merge (nutrition, ids, food):
     # inner merge of the 3 data frames.
     inner_merge = pd.merge(nutrition, ids, on="nutrient_id", how="inner")
-    usda = pd.merge(inner_merge, food, on="food_id", how="inner")
-    usda = usda[["food", "nutrient", "Per100g", "food_id", "nutrient_id" ]]
+    x = pd.merge(inner_merge, food, on="food_id", how="inner")
+    usda = x[["food", "nutrient", "Per100g", "food_id", "nutrient_id" ]]
     # cleaning of the ids columns.
     usda.drop(columns = ["food_id", "nutrient_id"], inplace= True, axis=1)
     # cleaning the duplicated.
     return usda
 
     
-def cleaning_merge (x):    
-    #cleaning duplicates and information
-    x.drop_duplicates(subset=['food', 'nutrient'], keep='first')
-    x["nutrient"] = [i.strip().replace("  ", " ") for i in list(x["nutrient"])]
-    x["food"] = [i.strip().replace("  ", " ") for i in list(x["food"])]
-    return x
-
-
-
 
 
 # web scraping
@@ -83,4 +74,34 @@ def db_concat (lst1, lst2, lst3):
     df3 = pd.DataFrame(df3[0].values.reshape(-1, 2), columns=['nutrients', 'info'])
     #concatinate the 3 database in 1
     return pd.concat([df1, df2, df3], axis=0)
+
+
+# create individual lists searching for the disease in each text.
+def diseases (x):
+    enferm = ["beriberi","weight loss","anaemia", "feeling weak and tired", "underdeveloped teeth and bones", "atherosclerosis", "weakness", "angular cheilitis", "chapped", "anemia", "cracked lips", "pellagra", "diarrhoea", "dermatitis", "dementia", "fatigue", "apathy", "irritability", "celiac disease", "crohn", "skin health", "neural tube defects", "methylmalonic acidemia", "scurvy", "skin spots", "nyctalopia", "keratomalacia", "blindness", "rickets", "weakening of bones", "decreased coagulation of blood", "muscle weakness", "nerve weakness", "osteopenia", "nausea", "vomiting", "kidney disease", "loss of taste, smell", "goitre", "hyponatremia", "dehydration", "heart diseases", "muscle contractions", "hypertension"]
+    disease = []
+    for i in enferm:
+        if i in x:
+            disease.append(i)
+    return (disease)
+
+def diseases_total (n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15,n16,n17,n18,n19,n20,n21,n22):
+    # adding all the diseases into one list.
+    lst = [n1]+[n2]+[n3]+[n4]+[n5]+[n6]+[n7]+[n8]+[n9]+[n10]+[n11]+[n12]+[n13]+[n14]+[n15]+[n16]+[n17]+[n18]+[n19]+[n20]+[n21]+[n22]
+    return lst
+    
+    
+    
+def df_diseases (lst):   
+    # adding it without a separetion between them.
+    nueva = []
+    for i in lst:
+        nueva.append("/ ".join(i))
+    # making it a data frame.
+    enf = pd.DataFrame(nueva)
+    return enf
+    
+def df_concat (df, enf):
+    # para concat las tablas
+    return pd.concat([df, enf ], axis=1, keys=["thiamine (vitamin b1)", "beriberi/ weight loss/ weakness"])
 
